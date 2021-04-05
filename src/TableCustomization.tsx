59d2b9f1b1b9a778295dc1264eeb6be89a4b0acf
ocavue <ocavue@gmail.com>
2021-04-05 09:08:27 +0800

    use tab to indent

--- src/TableCustomization.tsx
+++ src/TableCustomization.tsx
@@ -9,121 +9,121 @@ import { useCommands } from "@remirror/react-core";
 import React from "react";
 
 const useStyles = makeStyles((theme) => ({
-  popup: {
-    // ...theme.typography.body2,
-    // position: 'absolute',
-    // backgroundColor: 'white',
-    width: 320,
-    // boxShadow: theme.shadows[8],
-    // borderRadius: theme.shape.borderRadius,
-  },
+	popup: {
+		// ...theme.typography.body2,
+		// position: 'absolute',
+		// backgroundColor: 'white',
+		width: 320,
+		// boxShadow: theme.shadows[8],
+		// borderRadius: theme.shape.borderRadius,
+	},
 }));
 
 const ContextMenu: React.FC<TableCellMenuComponentProps> = ({
-  popupOpen,
-  setPopupOpen,
+	popupOpen,
+	setPopupOpen,
 }) => {
-  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+	const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
 
-  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    setAnchorEl(event.currentTarget);
-    setPopupOpen(true);
-  };
+	const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+		setAnchorEl(event.currentTarget);
+		setPopupOpen(true);
+	};
 
-  const handleClose = () => {
-    setAnchorEl(null);
-    setPopupOpen(false);
-  };
+	const handleClose = () => {
+		setAnchorEl(null);
+		setPopupOpen(false);
+	};
 
-  return (
-    <div>
-      <IconButton
-        aria-controls="simple-menu"
-        aria-haspopup="true"
-        size="small"
-        onClick={handleClick}
-      >
-        <ExpandMoreIcon />
-      </IconButton>
-      <MenuPopup
-        anchorEl={popupOpen ? anchorEl : null}
-        handleClose={handleClose}
-      />
-    </div>
-  );
+	return (
+		<div>
+			<IconButton
+				aria-controls="simple-menu"
+				aria-haspopup="true"
+				size="small"
+				onClick={handleClick}
+			>
+				<ExpandMoreIcon />
+			</IconButton>
+			<MenuPopup
+				anchorEl={popupOpen ? anchorEl : null}
+				handleClose={handleClose}
+			/>
+		</div>
+	);
 };
 
 const MenuPopup: React.FC<{
-  anchorEl: HTMLElement | null;
-  handleClose: () => void;
+	anchorEl: HTMLElement | null;
+	handleClose: () => void;
 }> = ({ anchorEl, handleClose }) => {
-  const commands = useCommands();
-  const classes = useStyles();
+	const commands = useCommands();
+	const classes = useStyles();
 
-  // close the popup after clicking
-  const handleClick = (command: () => void) => {
-    return () => {
-      command();
-      handleClose();
-    };
-  };
+	// close the popup after clicking
+	const handleClick = (command: () => void) => {
+		return () => {
+			command();
+			handleClose();
+		};
+	};
 
-  // Notice that we won't close the popup after changing the cell background
-  // because we want users to quick try multiple colors.
-  const setTableCellBackground = (color: string | null) => {
-    return () => {
-      commands.setTableCellBackground(color);
-    };
-  };
+	// Notice that we won't close the popup after changing the cell background
+	// because we want users to quick try multiple colors.
+	const setTableCellBackground = (color: string | null) => {
+		return () => {
+			commands.setTableCellBackground(color);
+		};
+	};
 
-  return (
-    <Menu
-      id="simple-menu"
-      anchorEl={anchorEl}
-      keepMounted
-      open={Boolean(anchorEl)}
-      classes={{ paper: classes.popup }}
-      onClose={handleClose}
-    >
-      <MenuItem button onClick={setTableCellBackground("red")}>
-        Highlight
+	return (
+		<Menu
+			id="simple-menu"
+			anchorEl={anchorEl}
+			keepMounted
+			open={Boolean(anchorEl)}
+			classes={{ paper: classes.popup }}
+			onClose={handleClose}
+		>
+			<MenuItem button onClick={setTableCellBackground("red")}>
+				Highlight
       </MenuItem>
-      <MenuItem button onClick={setTableCellBackground(null)}>
-        Remove cell color
-      </MenuItem>
-
-      <MenuItem button onClick={handleClick(commands.addTableRowBefore)}>
-        Insert row above
-      </MenuItem>
-      <MenuItem button onClick={handleClick(commands.addTableRowAfter)}>
-        Insert row below
-      </MenuItem>
-      <MenuItem button onClick={handleClick(commands.addTableColumnBefore)}>
-        Insert column before
-      </MenuItem>
-      <MenuItem button onClick={handleClick(commands.addTableColumnAfter)}>
-        Insert column after
+			<MenuItem button onClick={setTableCellBackground(null)}>
+				Remove cell color
       </MenuItem>
 
-      <MenuItem button onClick={handleClick(commands.deleteTableColumn)}>
-        Remove column
+			<MenuItem button onClick={handleClick(commands.addTableRowBefore)}>
+				Insert row above
       </MenuItem>
-      <MenuItem button onClick={handleClick(commands.deleteTableRow)}>
-        Remove row
+			<MenuItem button onClick={handleClick(commands.addTableRowAfter)}>
+				Insert row below
       </MenuItem>
-      <MenuItem button onClick={handleClick(commands.deleteTable)}>
-        Remove table
+			<MenuItem button onClick={handleClick(commands.addTableColumnBefore)}>
+				Insert column before
       </MenuItem>
-    </Menu>
-  );
+			<MenuItem button onClick={handleClick(commands.addTableColumnAfter)}>
+				Insert column after
+      </MenuItem>
+
+			<MenuItem button onClick={handleClick(commands.deleteTableColumn)}>
+				Remove column
+      </MenuItem>
+			<MenuItem button onClick={handleClick(commands.deleteTableRow)}>
+				Remove row
+      </MenuItem>
+			<MenuItem button onClick={handleClick(commands.deleteTable)}>
+				Remove table
+      </MenuItem>
+		</Menu>
+	);
 };
 
 export function TableCustomization() {
-  return (
-    <TableComponents
-      tableCellMenuProps={{
-        Component: ContextMenu,
-      }}
-    />
-  );
+	return (
+		<TableComponents
+			tableCellMenuProps={{
+				Component: ContextMenu,
+			}}
+		/>
+	);
 }
